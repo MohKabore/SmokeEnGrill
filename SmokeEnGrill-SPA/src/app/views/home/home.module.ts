e6bd9da66fc19e173f5e51cb5d15a5b0228d4106
moulot <gmoulot@gmail.com>
2021-08-01 22:38:13 +0000

    Upgrading_202108012238

--- SmokeEnGrill-SPA/src/app/views/home/home.module.ts
+++ SmokeEnGrill-SPA/src/app/views/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MdbSelectModule } from 'mdb-angular-ui-kit/select'
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MdbSelectModule
   ],
   declarations: [HomeComponent]
 })
--- SmokeEnGrill-SPA/src/app/views/home/home/home.component.ts
+++ SmokeEnGrill-SPA/src/app/views/home/home/home.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  options = [
+    { value: '1', label: 'One' },
+    { value: '2', label: 'Two' },
+    { value: '3', label: 'Three' },
+    { value: '4', label: 'Four' },
+    { value: '5', label: 'Five' },
+    { value: '6', label: 'Six' },
+    { value: '7', label: 'Seven' },
+    { value: '8', label: 'Eigth' },
+  ];
 
   constructor() { }
 
